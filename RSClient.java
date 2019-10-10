782352f3a2cfe6e12e602a45c096d01893d1d6eb
Endri <endri.gjura@outlook.com>
2019-10-10 16:44:18 +0200

    Update RSClient.java

--- RSClient.java
+++ RSClient.java
@@ -1,1 +1,42 @@
- 
+import java.io.*;
+import java.net.*;
+
+public class RSClient{
+
+	public static void main(String[] args){
+		InetAddress addr = null;
+		int port = -1;
+
+		try{
+			if(args.length == 3){
+				addr = InetAddress.getByName(args[0]);
+				port = Integer.parseInt(args[1]);
+			} else {
+				System.out.println("Usage: java RSClient IPDiscoveryServer portDS fileName")
+				System.exit(1);
+			}
+		} catch (UnknownHostException e){
+			System.out.println("Problemi nella determinazione dell'endpoint del server : ");
+			e.printStackTrace();
+			System.out.println("RSClientt: interrompo...");
+			System.exit(2);
+		}
+
+		DatagramSocket socket = null;
+		DatagramPacket packet = null;
+		byte[] buf = new byte[256];
+
+		try{
+			socket = new DatagramSocket();
+			socket.setSoTimeout(30000);
+			packet = new DatagramPacket(buf, buf.length, addr, port);
+			System.out.println("\nRSClient: avviato");
+			System.out.println("Creata la socket: " + socket);
+		} catch (SocketException e){
+			System.out.println("Problemi nella creazione della socket: ");
+			e.printStackTrace();
+			System.out.println("RSClient: interrompo...");
+			System.exit(1);
+		}
+	}
+}
